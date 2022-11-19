556edb54d4f86021d8b244c4ee3a0a2e9008ea7a
aleunamp <manuella.m.p182@gmail.com>
2022-11-18 22:17:57 -0300

    feat: sing-up and sing-in without  token

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,8 +2,82 @@ import joi from "joi";
 import bcrypt from "bcrypt";
 import { v4 as uuidV4 } from 'uuid';
 
-export async function postSingIn (req, res){};
-//{nome, email, senha, confirme a senha}
+const userUpSchema = joi.object({
+    name: joi.string().min(3).required().trim(),
+    email: joi.required().email().string().trim(),
+    password: joi.required(),
+    passwordConfirm: joi.required()
+});
 
-export async function postSingUp (req, res){};
-//{email, senha}
+const userInSchema = joi.object({
+    email: joi.required().email().string().trim(),
+    password: joi.required()
+});
+
+export async function postSingUp(req, res) {
+
+    const user = req.body;
+
+    try {
+        const userExists = await userCollection.findOne({ email: user.email });
+
+        if (userExists) {
+            res.status(409).send({ message: "Esse e-mail já está cadastrado!" });
+            return;
+        };
+
+        const validationStatus = userUpSchema.validate(req.body, { abortEarly: false });
+
+        if (validationStatus.error) {
+            const error = validationStatus.error.details.map((detail) => detail.message);
+            res.status(422).send(error);
+            return;
+        };
+
+        if(user.password !== user.passwordConfirm){
+            res.status(400).send({message: "As senhas digitadas são diferentes!"});
+            return;
+        };
+
+        const hashPassword = bcrypt.hashSync(user.password, 10);
+
+        await userCollection.insertOne({ ...user, password: hashPassword });
+        res.status(201).send({ message: "Cadastro realizado com sucesso!" });
+    } catch (error) {
+        res.status(500).send(error);
+    };
+
+};
+
+export async function postSingIn(req, res) {
+
+    const { email, password } = req.body;
+
+    try {
+        const userExists = await userCollection.findOne({ email });
+
+        if(!userExists){
+            res.status(401).send({message: "O e-mail não está cadastrado!"});
+            return;
+        };
+
+        const validationStatus = userInSchema.validate(req.body, { abortEarly: false });
+
+        if (validationStatus.error) {
+            const error = validationStatus.error.details.map((detail) => detail.message);
+            res.status(422).send(error);
+            return;
+        };
+
+        const passwordOk = bcrypt.compareSync(password, userExists.password);
+
+        if(!passwordOk){
+            res.status(401).send({message: "A senha está incorreta!"});
+        };
+
+        res.send({message: `Olá ${userExists.name}, seja bem-vinde!`});
+    } catch (error) {
+        res.status(500).send(error);
+    }
+
+};
